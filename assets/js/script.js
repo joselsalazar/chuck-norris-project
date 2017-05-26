6214ddb14f682483f089fd530c669d2d2ea035b1
Jose Salazar <jose.salazar.home@gmail.com>
2017-05-25 20:04:35 -0700

    JS changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-alert("test");
+var queryURL = "https://api.chucknorris.io/jokes/random?query=";
+var query = "";
+
+$("#submit-query").click(function() {
+  event.preventDefault();
+  query = $('#query').val().trim();
+  queryURL = "https://api.chucknorris.io/jokes/random?query=" + query;
+  runAJAX();
+})
+
+function runAJAX() {
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .done(function(response) {
+    console.log(response);
+    $('.answer-p').html(response.value);
+  });
+}
