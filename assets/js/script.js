8c85d41616eeda14ff8301563e3ec0831b5a7f16
Jose Salazar <jose.salazar.home@gmail.com>
2017-05-29 10:56:10 -0700

    Finished Search and Added Censor Filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 var queryURL = "";
 var query = "";
 var randomQuery = [];
+var chuckAnswer = "";
 
-$("#submit-query").click(function() {
+function sendToAjax() {
   event.preventDefault();
   query = $('#query').val().trim();
   queryURL = "https://api.chucknorris.io/jokes/search?query=" + query;
+  backupQueryURL = "https://api.chucknorris.io/jokes/random";
   runAJAX();
-})
+}
 
 function runAJAX() {
 $.ajax({
@@ -15,13 +17,48 @@ $.ajax({
     method: "GET"
   })
   .done(function(response) {
-    for (var i=0; i<response.result.length; i++){
-      var responses = response.result[i].value;
-      randomQuery.push(responses);
+    console.log(response);
+    if(response.result === null) {
+      console.log("this is blank");
+      $.ajax({
+        url: backupQueryURL,
+        method: "GET"
+      })
+      .done(function(response) {
+        console.log(response);
+        var chuckAnswer = response.value;
+        $('.answer-p').html(chuckAnswer);
+      })
+    } else {
+      for (var i=0; i<response.result.length; i++){
+        var responses = response.result[i].value;
+        randomQuery.push(responses);
+      }
+      var chuckAnswer = randomQuery[Math.floor(Math.random() * randomQuery.length)];
+      console.log(chuckAnswer);
+      $('.answer-p').html(chuckAnswer);
     }
-    
-    var chuckAnswer = randomQuery[Math.floor(Math.random() * randomQuery.length)];
-    console.log(chuckAnswer);
-    $('.answer-p').html(chuckAnswer);
+
+    String.prototype.repeat = function(num){
+      return new Array(num + 1).join(this);
+    }
+
+    var filter = ['cock', 'fuck', 'fucks', 'fucking', 'motherfucking', 'fucked-up'];
+
+    $('.answer-p').text(function(i, txt){
+      // iterate over all words
+      for(var i=0; i<filter.length; i++){
+        // Create a regular expression and make it global
+        var pattern = new RegExp('\\b' + filter[i] + '\\b', 'g');
+        // Create a new string filled with '*'
+        var replacement = '*'.repeat(filter[i].length);
+        txt = txt.replace(pattern, replacement);
+      }
+
+      // returning txt will set the new text value for the current element
+      return txt;
+    });
   });
 }
+
+// To Do
