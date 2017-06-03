e89e07985733a6505ecb1dc0800c6003f462ecbe
Jose Salazar <jose.salazar.home@gmail.com>
2017-06-03 12:54:55 -0700

    Morechanges, Forum Funcionality

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -14,55 +14,39 @@ var database = firebase.database();
 
 // Initial Variables (SET the first set IN FIREBASE FIRST)
 // Note remember to create these same variables in Firebase!
-var employee = "";
-var role = "";
-var startDate = "";
-var monthlyRate = "";
+var post = "";
+var postTitle = "";
+var userId = "post-";
+var e = 0;
 
 // Click Button changes what is stored in firebase
-$("#submit-employee").on("click", function() {
+$("#submit-post").on("click", function() {
   // Prevent the page from refreshing
   event.preventDefault();
 
   // Get inputs
-  employee = $("#employee").val().trim();
-  role = $("#role").val().trim();
-  startDate = $("#start-date").val().trim();
-  monthsWorked = $("#months-worked").val().trim();
-  monthlyRate = $("#monthly-rate").val().trim();
+  postTitle = $("#title-post").val().trim();
+  post = $("#post").val().trim();
+
+  // Store e in firebase??
+
+  function ePush() {
+    e++;
+  }
 
   // Change what is saved in firebase
-  database.ref().push({
-    employee: employee,
-    role: role,
-    startDate: startDate,
-    monthsWorked: monthsWorked,
-    monthlyRate: monthlyRate
+  database.ref('norris-post/' + userId + e).push({
+    postTitle: postTitle,
+    post: post
   });
 });
 
 // Firebase is always watching for changes to the data.
 // When changes occurs it will print them to console and html
-database.ref().on("child_added", function(snapshot) {
+database.ref("norris-post").on("child_added", function(snapshot) {
 
   // Print the initial data to the console.
   console.log(snapshot.val());
-
-  var newRow = $('<tr>');
-  $('.employee-form').append(newRow);
-  newRow.append("<td>" + snapshot.val().employee + "</td>");
-  newRow.append("<td>" + snapshot.val().role + "</td>");
-  newRow.append("<td>" + snapshot.val().startDate + "</td>");
-  newRow.append("<td>" + snapshot.val().monthsWorked + "</td>");
-  newRow.append("<td>" + snapshot.val().monthlyRate + "</td>");
-  newRow.append("<td>" + parseInt(snapshot.val().monthlyRate) * parseInt(snapshot.val().monthsWorked) + "</td>");
-
-  // Log the value of the various properties
-  console.log(snapshot.val().employee);
-  console.log(snapshot.val().role);
-  console.log(snapshot.val().startDate);
-  console.log(snapshot.val().monthsWorked);
-  console.log(snapshot.val().monthlyRate);
 
   // If any errors are experienced, log them to console.
 }, function(errorObject) {
